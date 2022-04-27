7a4619e1c40870e3f8fc34f40f61600e9d86349e
aliraad79 <a.ahmadi.k.79@gmail.com>
2022-04-27 13:44:15 +0430

    Add employee restaurant foreign keys

--- main.sql
+++ main.sql
@@ -1,13 +1,26 @@
-drop table if exists restaurant, employee, employee, inspector, ingredients, food_item, bill cascade;
-drop domain if exists d_type, d_position, f_type, b_type;
+drop table if exists restaurant,
+employee,
+employee,
+inspector,
+ingredients,
+food_item,
+bill cascade;
+
+drop domain if exists d_type,
+d_position,
+f_type,
+b_type;
 
 create domain d_type char(2) check (value in ('bs', 'ms', 'dr'));
+
 create domain d_position char(3) check (value in ('bs1', 'ms1', 'dr1'));
+
 create domain f_type char(3) check (value in ('fs1', 'mf1', 'fr1'));
+
 create domain b_type char(5) check (value in ('gas', 'water', 'plum'));
 
 create table restaurant (
-	rid int primary key,
+    rid int primary key,
     name varchar(32) not null,
     capacity int not null,
     address varchar(256) not null,
@@ -15,19 +28,16 @@ create table restaurant (
 );
 
 create table employee (
-	eid int primary key,
+    eid int primary key,
     name varchar(32),
     type d_type default 'bs',
-    position d_position default 'ms1'
+    position d_position default 'ms1',
+    working_restaurant int references restaurant(rid) on delete cascade
 );
 
-create table inspector (
-    name varchar(32) primary key
-);
+create table inspector (name varchar(32) primary key);
 
-create table ingredients (
-    name varchar(32) primary key
-);
+create table ingredients (name varchar(32) primary key);
 
 create table food_item (
     name varchar(32),
@@ -40,4 +50,11 @@ create table food_item (
 create table bill (
     type b_type primary key,
     quantity int not null
+);
+
+
+create table change_restaurant (
+    target_eid int,
+	eid int references employee(eid) on delete cascade,
+	rid int references restaurant(rid) on delete cascade
 );
